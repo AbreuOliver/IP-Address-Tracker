bc7293a75e58ff80a81b05b24c2b362ff688b741
Isla <fallenacademy@gmail.com>
2020-09-28 14:58:39 -0700

    tried require js and not sure how this works

--- src/app.js
+++ src/app.js
@@ -1,5 +1,7 @@
-const axios = require('axios')
+var require = require('requirejs');
 
+const axios = require('axios')
+console.log(axios)
 import {map} from './api/map.js';
 import {getRequest} from './api/ip.js'
 
